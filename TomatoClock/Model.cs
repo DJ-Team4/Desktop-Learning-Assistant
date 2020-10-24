8004fb30f7f9b12224177041b316730d4726605f
Sylvia Fu <875275641@qq.com>
2020-10-24 15:01:07 +0800

    TomatoClock 框架

---
+++ TomatoClock/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomatoClock.TomatoTime;
+
+namespace TomatoClock.Model
+{
+    public class TaskInfo
+    {
+        public string Name { get; set; }
+        public string Remark { get; set; }
+        public DateTime StartTime { get; set; }
+        public Time SetTime { get; set; }
+        public Time SetSpan { get; set; }
+        public Time LeftTime { get; set; }
+    }
+}
---
+++ TomatoClock/TomatoClockService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomatoClock.Model;
+
+namespace TomatoClock
+{
+    class TomatoClockService
+    {
+        private List<TaskInfo> TaskRecords;
+
+        public void AddTask() { }
+        public void DeletTask() { }
+        public void ModifyTask() { }
+        public void ShowTask() { }
+    }
+}
