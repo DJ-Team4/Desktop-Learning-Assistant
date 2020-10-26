71a6e40c954ffdf6a3def7c0d756867ae82fcd62
Sylvia Fu <875275641@qq.com>
2020-10-26 14:36:04 +0800

    Add TomatoClock Class and TomatoClockConfig

--- Desktop-Learning-Assistant/TomatoClock/TomatoClockService.cs
+++ Desktop-Learning-Assistant/TomatoClock/TomatoClockService.cs
@@ -7,11 +7,22 @@ using TomatoClock.Model;
 
 namespace TomatoClock
 {
-    class TomatoClockService
+    class TaskService
     {
-        private List<TaskInfo> TaskRecords;
 
-        public void AddTask() { }
+        public void AddTask()
+        {
+            TaskInfo NewTask = new TaskInfo();
+            Console.WriteLine("Task Infomation: \n");
+            NewTask.Name = Console.ReadLine();
+
+            Console.WriteLine("Set Start Time: \n"+"eg:For 2020/10/26 8:39:00, Input 2020102684100 ");
+            string SetStartLine = Console.ReadLine();
+            //NewTask.SetTime = DateTime.ParseExact(SetStartLine, "yyyyMMddhhmmss", System.Globalization.CultureInfo.CurrentCulture);
+
+            Console.WriteLine("Set Time Span: ");
+
+        }
         public void DeletTask() { }
         public void ModifyTask() { }
         public void ShowTask() { }
