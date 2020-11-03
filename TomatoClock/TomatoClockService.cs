2cf83511e625e2f04c7481f83c61b92453814c7d
Sylvia Fu <875275641@qq.com>
2020-11-03 21:17:40 +0800

    Add method ReadTomato()

--- TomatoClock/TomatoClockService.cs
+++ TomatoClock/TomatoClockService.cs
@@ -119,79 +119,25 @@ namespace DesktopLearningAssistant.TomatoClock.SQLite
                 context.SaveChanges();
             }
         }
-        public int ReadTomato(int TaskID)
+        public List<Tomato> ReadTomato(int iTaskID)    //提供id为TaskID的所有番茄钟信息（起止时间）以查找时段内的应用程序信息
         {
-            int count = 0;
-            return count;
-        }
-        private void AddTomatoNum()
-        {
-
-        }
-        /*
-        public bool AddTask(TaskInfo task)
-        {
-            string sql = "INSERT INTO Task(name,StartTime,Deadline,notes,TomatoNum,TomatoCount,State)" +
-                "values(@name,@StartTime,@Deadline,@notes,@TomatoNum,@TomatoCount,@State)";
-            SQLiteHelper DB = new SQLiteHelper("D:/TaskInfo.db");
-            SQLiteParameter[] parameters = new SQLiteParameter[]{
-            new SQLiteParameter("@name",task.Name.ToString()),
-            new SQLiteParameter("@StartTime",task.StartTime.ToString()),
-            new SQLiteParameter("@Deadline",task.Deadline),
-            new SQLiteParameter("@notes",task.Notes.ToString()),
-            new SQLiteParameter("@TomatoNum",0),
-            new SQLiteParameter("@TomatoCount",0),
-            new SQLiteParameter("@State",task.TaskState.ToString())
-            };
-            DB.ExecuteNonQuery(sql, parameters);
-            return true;
-        }
-        public void DeletTask(TaskInfo task)
-        {
-            SQLiteHelper DB = new SQLiteHelper("D:/TaskInfo.db");
-            string sql = "DELETE FROM Task WHERE TaskID = '" + task.TaskID + "'";
-            DB.ExecuteNonQuery(sql, null);
-            Console.WriteLine(task.TaskID + "被删除！");
-        }
-        public bool ModifyTask(TaskInfo task)
-        {
-            ReadTask(task.TaskID);
-            AddTask(task);
-
-            return true;
-        }
-        public TaskInfo ReadTask(int TaskID)
-        {
-            TaskInfo taskInfo = new TaskInfo();
-            string sql = "SELECT * FROM Task WHERE TaskID = '" + TaskID + "'";
-            SQLiteHelper DB = new SQLiteHelper("D:/TaskInfo.db");
-            SQLiteDataReader dr = DB.ExecuteReader(sql, null);
-            while(dr.Read())
+            List<Tomato> TomatoList = new List<Tomato>();
+            using (var context = new TaskTomatoContext())
             {
-                taskInfo.TaskID = (int)dr["TaskID"];
-                taskInfo.Name = dr["name"].ToString();
-                taskInfo.StartTime = 
+                var query = context.TaskTomatoes.Where(tt => tt.TaskList.TaskID == iTaskID).OrderBy(tt => tt.TomatoID);
+                foreach (var tt in query)
+                {
+                    Tomato tomato = new Tomato();
+                    tomato.StartTime = tt.BeginTime;
+                    tomato.EndTime = tt.EndTime;
+                    TomatoList.Add(tomato);
+                }
+                return TomatoList;
             }
-            return taskInfo;
-        }
-        public int AddTomatoStartTime(int TaskID)
-        {
-            int TomatoID = 0;
-            return TomatoID;
-        }
-        public void AddTomatoEndTime(int TaskID, int TomatoID)
-        {
-
-        }
-        public int ReadTomato(int TaskID)
-        {
-            int count = 0;
-            return count;
         }
         private void AddTomatoNum()
         {
 
         }
-        */
     }
 }
