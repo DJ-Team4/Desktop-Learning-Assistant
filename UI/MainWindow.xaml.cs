2b092decd82b41bd6eae8998e0feecfd5ad399d8
LONG-xy <1260269038@qq.com>
2020-11-08 19:51:18 +0800

    25min倒计时

--- UI/MainWindow.xaml.cs
+++ UI/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -18,6 +19,7 @@ using Panuon.UI.Silver;
 using LiveCharts;
 using LiveCharts.Wpf;
 using UI.Process;
+using System.ComponentModel;
 
 namespace UI
 {
@@ -38,7 +40,8 @@ namespace UI
         {
             InitializeComponent();
             
-            Debug.WriteLine("hello");
+            this.DataContext = new ViewModel();
+
             this.Loaded += new RoutedEventHandler(TomatoClock_OnLoaded); //***加载倒计时
 
             PointLabel = chartPoint =>
@@ -53,6 +56,103 @@ namespace UI
             };
             DataContext = this;
         }
+/// <summary>
+/// Button点击更改按钮背景
+/// </summary>
+        public class ViewModel : INotifyPropertyChanged
+        {
+            private bool _isPlaying = true;
+            private RelayCommand _playCommand;
+
+            public ViewModel()
+            {
+                isPlaying = true;
+            }
+
+            public bool isPlaying
+            {
+                get { return _isPlaying; }
+                set
+                {
+                    _isPlaying = value;
+                    OnPropertyChanged("isPlaying");
+                }
+            }
+
+            public ICommand PlayCommand
+            {
+                get
+                {
+                    return _playCommand ?? new RelayCommand((x) =>
+                    {
+                        var buttonType = x.ToString();
+
+                        if (null != buttonType)
+                        {
+                            if (buttonType.Contains("Start"))
+                            {
+                                isPlaying = true;
+                            }
+                            else if (buttonType.Contains("Pause"))
+                            {
+                                isPlaying = false;
+                            }
+                        }
+                    });
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+            public void OnPropertyChanged(string propertyName)
+            {
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                }
+            }
+        }
+
+        public class RelayCommand : ICommand
+        {
+            private readonly Predicate<object> _canExecute;
+            private readonly Action<object> _execute;
+
+            public event EventHandler CanExecuteChanged;
+
+            public RelayCommand(Action<object> execute) : this(execute, null) { }
+
+            public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+            {
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+
+                if (_canExecute == null)
+                {
+                    return true;
+                }
+
+                return _canExecute(parameter);
+            }
+
+            public void Execute(object parameter)
+            {
+                _execute(parameter);
+            }
+
+            public void RaiseCanExecuteChanged()
+            {
+                if (CanExecuteChanged != null)
+                {
+                    CanExecuteChanged(this, EventArgs.Empty);
+                }
+            }
+        }
+
+        //***********************
 
         public Func<ChartPoint, string> PointLabel { get; set; }
 
@@ -84,7 +184,7 @@ namespace UI
             //处理倒计时的类
             timeCount = new TimeCount(hour * 3600 + minute * 60 + second);
             CountDown += new CountDownHandler(timeCount.TimeCountDown);
-            timer.Start();
+           //  timer.Start();
         }
 
         /// <summary>
@@ -134,6 +234,25 @@ namespace UI
             Debug.WriteLine("drop");
             TagWindow tagWindow=new TagWindow();
             tagWindow.Show();
+        }
+
+      
+
+        private void TimeCountStart_OnClick(object sender, RoutedEventArgs e)
+        {
+
+ 
+            timer.Start();
+           ImageSource pause = new BitmapImage(new Uri("Icon/Pause.jpg", UriKind.Relative));
+
+           this.ButtonImage.Source = pause;
+
+
+        }
+
+        private void TimeCountPause_Click(object sender, MouseButtonEventArgs e)
+        {
+            timer.Stop();
         }
     }
 }
