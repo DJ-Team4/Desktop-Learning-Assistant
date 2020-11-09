cadcde549c690cd1a0b90fa537c4c8fae61c5c94
Photooon <1527842029@qq.com>
2020-11-09 08:42:38 +0800

    解决UI项目的库问题

--- UI/App.xaml.cs
+++ UI/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using DesktopLearningAssistant.TimeStatistic;
 
 namespace UI
 {
@@ -13,5 +14,27 @@ namespace UI
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 程序入口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        /// <summary>
+        /// 程序出口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            // 将屏幕时间统计数据写入DB
+            TimeDataManager timeDataManager = TimeDataManager.GetTimeDataManager();
+            timeDataManager.SaveDataToDb();
+        }
     }
 }
