4b93f97efbcc8068f374b9c5c15195eae5f58de7
Sylvia Fu <875275641@qq.com>
2020-11-10 22:21:58 +0800

    Add AppInfo datastruct to provide to UI

---
+++ Desktop-Learning-Assistant/TomatoClock/Model/AppInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopLearningAssistant.TomatoClock.Model
+{
+    public class AppInfo
+    {
+        public string TaskName { get; set; }
+        public TimeSpan AppTime { get; set; }
+        public TimeSpan AllTIme { get; set; }
+    }
+}
